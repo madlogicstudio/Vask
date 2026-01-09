545a0fadadeb1f9ca77bc2f6ad7ce2869b2eb8ae
Yureusu <madlogicstuiox@gmail.com>
2026-01-09 22:56:19 +0800

    fixed app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
+import { useEffect, useState } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { auth } from "./firebase/FirebaseConfig";
+
+function AppWrapper() {
+  const navigate = useNavigate();
+  const [checkingRedirect, setCheckingRedirect] = useState(true);
+
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result?.user) {
+          console.log("Google redirect login success", result.user);
+          navigate("/dashboard");
+        }
+      })
+      .catch(console.error)
+      .finally(() => setCheckingRedirect(false));
+  }, [navigate]);
+
+  if (checkingRedirect) {
+    return <div>Loading...</div>;
+  }
+
+  return <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+  </Routes>;
+}
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <AppWrapper />
     </HashRouter>
   );
 }
