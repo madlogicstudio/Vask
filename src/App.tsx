e6e75651f56d7d19ddbfdc679721c9ec9df8ccf1
Yureusu <madlogicstuiox@gmail.com>
2026-01-10 00:09:32 +0800

    change to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,43 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { getRedirectResult, onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/FirebaseConfig";
 
+
 function AppWrapper() {
-    const navigate = useNavigate();
     const [checkingRedirect, setCheckingRedirect] = useState(true);
     const [user, setUser] = useState<any>(null);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            if (currentUser) {
-                setUser(currentUser);
-            }
+            if (currentUser) setUser(currentUser);
         });
-
+    
         getRedirectResult(auth)
             .then((result) => {
-                if (result?.user) {
-                    console.log("Google redirect login success", result.user);
-                    setUser(result.user);
-                }
+                if (result?.user) setUser(result.user);
             })
             .catch(console.error)
             .finally(() => setCheckingRedirect(false));
-
+    
         return unsubscribe;
     }, []);
-
-    useEffect(() => {
-        if (!checkingRedirect && user) {
-            navigate("/dashboard");
-        }
-    }, [user, checkingRedirect, navigate]);
+    
 
     if (checkingRedirect) return <div>Loading...</div>;
-    if (user) return <div>Redirecting to dashboard...</div>;
+    if (user) return <Dashboard />; 
     return <Landing />;
 }
 
-function App() {
+export default function App() {
     return (
-        <BrowserRouter basename="/Vask">
+        <HashRouter>
+            <AppWrapper />
             <Routes>
-                <Route path="/" element={<AppWrapper />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
-
-export default App;
