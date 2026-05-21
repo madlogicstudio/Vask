5f7749c265f4c5dee0bf47da82c01e58375e8ecd
Jutsy <madlogicstudiox@gmail.com>
2026-05-22 02:13:32 +0800

    Fixed some bugs

--- app/components/Cookie.tsx
+++ app/components/Cookie.tsx
@@ -13,22 +13,22 @@ const Cookie = ({isDark, setIsVisible}: CookieProps) => {
 
     return (
         <div className={`${isDark ? "text-[var(--light-color)] bg-[var(--dark-color)]" : "bg-[var(--light-color)] text-[var(--dark-color)]"}
-            ${isMobile? "p-[calc(0.6vw+0.4rem)]" : ""}
+            ${isMobile? "p-[calc(0.6vw+0.4rem)]" : "p-[calc(0.6vw+0.4rem)]"}
             h-auto w-full flex flex-col items-start justify-start  fadeIn`}>
             
             <div className={`${isMobile? "flex-col" : "flex-row items-center justify-between"}
                 w-full flex gap-[calc(0.6vw+0.4rem)]`}>
-                <span className='text-[length:var(--medium-font)] font-semibold cursor-pointer transition duration-300 ease-in-out'>
+                <span className='text-[length:var(--small-font)] font-semibold cursor-pointer transition duration-300 ease-in-out'>
                     We value your privacy
                 </span>
                 <div className={`${isMobile? "justify-start py-[calc(0.6vw+0.4rem)]" : "justify-center"}
                     flex items-center gap-[0.5em]`}>
-                    <span className="px-[calc(0.6vw+0.4rem)] py-[calc(0.4vw+0.3rem)] text-[length:var(--small-font)] rounded-full text-white bg-black cursor-pointer
+                    <span className="px-[calc(1.2vw+0.8rem)] py-[calc(0.4vw+0.3rem)] text-[length:var(--extrasmall-font)] rounded-full text-white bg-black cursor-pointer
                         hover:text-[color:var(--purple-color)] hover:bg-gray-300 transition duration-300 ease-in-out"
                         onClick={() => setIsVisible(false)}>
                         Accept all
                     </span>
-                    <span className="px-[calc(0.6vw+0.4rem)] py-[calc(0.4vw+0.3rem)] text-[length:var(--small-font)] rounded-full text-white bg-black cursor-pointer
+                    <span className="px-[calc(1.2vw+0.8rem)] py-[calc(0.4vw+0.3rem)] text-[length:var(--extrasmall-font)] rounded-full text-white bg-black cursor-pointer
                         hover:text-[color:var(--purple-color)] hover:bg-gray-300 transition duration-300 ease-in-out"
                         onClick={() => setIsVisible(false)}>
                         Reject all
@@ -37,11 +37,11 @@ const Cookie = ({isDark, setIsVisible}: CookieProps) => {
             </div>
             <div className={`${isMobile? "flex-col items-start" : "flex-row items-center"}
                 w-full flex justify-start gap-[calc(0.4vw+0.3rem)]`}>
-                <span className='text-[length:var(--small-font)] cursor-pointer transition duration-300 ease-in-out'>
+                <span className='text-[length:var(--extrasmall-font)] cursor-pointer transition duration-300 ease-in-out'>
                     This website uses cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. 
                     By clicking "Accept all" you consent to our use of cookies.
                 </span>
-                <span className='text-[length:var(--small-font)] text-blue-500 underline cursor-pointer transition duration-300 ease-in-out'>
+                <span className='text-[length:var(--extrasmall-font)] text-blue-500 underline cursor-pointer transition duration-300 ease-in-out'>
                     Cookie Policy
                 </span>
             </div>
